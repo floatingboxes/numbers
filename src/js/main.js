a7aa86f25be8fa418148d51d765293fbfcc1fa0a
Dan Sweeney <dan@floatingboxes.com>
2015-06-08 10:04:49 -0500

    form styling

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,8 @@ $(document).ready(function(){
 
   // Data from Google Spreadsheets for Events
   $.get("https://gridspree.io/ss/AxznvuNqkuT4it53fVoGRP", function(data) {
-    console.log(data.rows[0].Bal);
-    $(".js-number").append(data.rows[0].Bal);
+    console.log(data.rows[0].bal);
+    $(".js-number").append(data.rows[0].bal);
     $(".js-number-loading").remove();
   });
 });
-
-
